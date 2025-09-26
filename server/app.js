8f07412a7f9e20bd3dcfbb71755519ea1f6e8584
Mohammed Alam <ma3655@rit.edu>
2025-09-26 02:04:56 -0400

    readme and main content

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + "/public/index.html")
+})
+app.get('/character/1', (req, res) => {
+    res.sendFile(process.cwd() + "/public/character/max.html")
+})
+app.get('/character/2', (req, res) => {
+    res.sendFile(process.cwd() + "/public/character/shara.html")
+})
+app.get('/character/3', (req, res) => {
+    res.sendFile(process.cwd() + "/public/character/marden.html")
+})
+app.get('/character/4', (req, res) => {
+    res.sendFile(process.cwd() + "/public/character/casitra.html")
+})
+app.get('/character/5', (req, res) => {
+    res.sendFile(process.cwd() + "/public/character/sparrow.html")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
